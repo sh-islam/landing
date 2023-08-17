fb8e0132b478c7344132cc6c7bf5ca7f9860a789
sh-islam <misla2@uwo.ca>
2023-08-17 02:29:38 -0400

    added link to github

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,7 @@
 function About(){
     return(
         <div className="about-container">
-            <p>Below you can see some of my projects. Other projects and code snippets can be viewed at my github</p>
+            <p>Below you can see some of my projects. Other projects and code snippets can be viewed on my <a href="https://github.com/sh-islam">github</a>.</p>
         </div>
     )
 }
